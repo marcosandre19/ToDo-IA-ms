9fdaefc7f08eecd444a507618a9378d7439e7eb6
marcosandre19@gmail.com <marcosandre@pactosolucoes.com.br>
2026-05-03 15:15:59 -0300

    test(setup): adicionar smoke test contextLoads
    Valida que o ApplicationContext do Spring Boot sobe sem erro contra o H2 em modo arquivo. Único teste da etapa de setup; testes de domínio virão junto com as entidades e repositórios.

---
+++ src/test/java/com/toDo/tarefas/TarefasApplicationTests.java
@@ -0,0 +1,12 @@
+package com.toDo.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
